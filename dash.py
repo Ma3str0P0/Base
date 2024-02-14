787b37f79c3d7a45b3baa0dad3311939c42681a5
Ma3str0P0 <73281011+Ma3str0P0@users.noreply.github.com>
2024-02-14 02:08:06 -0500

    Update dash.py

--- dash.py
+++ dash.py
@@ -10,9 +10,6 @@ texto = """
 -
 -
 -
--
--
--
 Análisis: La última tensión máxima observada es de 3.092 voltios de SKY_MTY S1A_BCU01_VOLT_MIN, y la última tensión mínima es de 2.985 voltios de SKY_MTY S1A_BCU09_VOLT_MIN, lo que resulta en una variación de voltaje de 0.107 voltios.
 Diagnóstico: La variación de voltaje excede el umbral de 0.05 voltios durante el proceso de descarga de la batería, ya que el SOC está entre el 10% y el 50%. El sistema no está operando dentro del rango aceptable de equilibrio de voltaje, lo que podría llevar a una detención prematura en la descarga.
 
