9701af802378ec14225958cc5a125aea502969bd
Ma3str0P0 <73281011+Ma3str0P0@users.noreply.github.com>
2024-02-14 02:05:15 -0500

    Update dash.py

--- dash.py
+++ dash.py
@@ -5,11 +5,13 @@ st.title("SCAN ON")
 
 # Texto
 texto = """
-# Título del Texto
-
-Este es un ejemplo de texto en **Markdown** para un dashboard de Streamlit.
-
-Aquí puedes escribir todo el texto que desees. El área de desplazamiento vertical se activará automáticamente si el contenido es demasiado largo para caber en la pantalla.
+# Análisis: La última tensión máxima observada es de 3.092 voltios de SKY_MTY S1A_BCU01_VOLT_MIN, y la última tensión mínima es de 2.985 voltios de SKY_MTY S1A_BCU09_VOLT_MIN, lo que resulta en una variación de voltaje de 0.107 voltios.
+Diagnóstico: La variación de voltaje excede el umbral de 0.05 voltios durante el proceso de descarga de la batería, ya que el SOC está entre el 10% y el 50%. El sistema no está operando dentro del rango aceptable de equilibrio de voltaje, lo que podría llevar a una detención prematura en la descarga.
+Severidad del desequilibrio: 7/10
+Probabilidad de desequilibrio de rack: 1.0
+Acciones sugeridas: 1. Iniciar un procedimiento de equilibrio para corregir las diferencias de voltaje. 2. Inspeccionar SKY_MTY S1A_BCU09 en busca de posibles problemas que causen la alta caída de voltaje.
+Enviar mensaje al operador: sí
+SKY_MTY S1A
 """
 
 # Imagen
@@ -20,7 +22,7 @@ Aquí puedes escribir todo el texto que desees. El área de desplazamiento verti
 st.markdown(texto)
 
 # Input de texto
-#texto = st.text_input("Ingresa tu consulta aquí.")
+texto = st.text_input("Ingresa tu consulta aquí.")
 
 # Input de archivo
 #archivo = st.file_uploader("Cargar archivo", type=["csv", "txt"])
